ca23f3d79ba94d7daef092d4e508ede89ed96e07
seul98 <totolo402@naver.com>
2025-07-03 11:15:15 +0900

    asd

---
+++ src/main/java/com/example/demo/AaasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaasApplication.class, args);
+	}
+
+}
